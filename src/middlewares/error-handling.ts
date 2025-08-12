9ce61f794b5b62dd974a18b580aca9cc7b8de2ed
eomgn <matheeusgn21@gmail.com>
2025-08-12 14:31:31 -0300

    :sparkles: feat: instalando o zod para lidar com validacoes e implementando no error handling para gerenciar error da validacao com o zod

--- src/middlewares/error-handling.ts
+++ src/middlewares/error-handling.ts
@@ -3,6 +3,9 @@ import { Request, Response, NextFunction } from "express";
 //  AppError
 import { AppError } from "@/utils/AppError";
 
+// ZodError
+import { ZodError } from "zod";
+
 export function errorHandling(
   error: any,
   request: Request,
@@ -12,6 +15,13 @@ export function errorHandling(
   if (error instanceof AppError) {
     // cada error gerado pela instância de AppError retornando então o status e mensagem passada
     return response.status(error.statusCode).json({ message: error.message });
+  }
+
+  //  gerenciar erros de validação com Zod
+  if (error instanceof ZodError) {
+    return response
+      .status(400)
+      .json({ message: "validation error", issues: error.format });
   }
 
   // erro generico, são sendo uma instancia de AppError
