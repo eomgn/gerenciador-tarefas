dd0ee3d7855d4eca837adc7554571b307b76868b
eomgn <matheeusgn21@gmail.com>
2025-08-13 15:17:35 -0300

    :sparkles: feat: concluido o método async para então ser possivel criar um novo usuario passando pelas validacoes --- tambem criado uma nova pasta database com a configuracao para o prisma client

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -1,9 +1,11 @@
 import { Request, Response, NextFunction } from "express";
-
 import zod from "zod";
+import { hash } from "bcrypt";
+import { prisma } from "@/database/prisma";
+import { AppError } from "@/utils/AppError";
 
 export class UsersController {
-  create(request: Request, response: Response, next: NextFunction) {
+  async create(request: Request, response: Response, next: NextFunction) {
     const bodySchema = zod.object({
       name: zod.string().trim().min(3),
       email: zod.email(),
@@ -12,8 +14,25 @@ export class UsersController {
 
     const { name, email, password } = bodySchema.parse(request.body);
 
-    return response.json({
-      message: "Usuário criado.",
+    // ----- criando uma hash para password -----
+    const hashPassword = await hash(password, 8);
+
+    // ----- verificando se já existe um usuário com o e-mail passado no corpo da requisição -----
+    const userAlreadyEmail = await prisma.user.findUnique({ where: { email } });
+
+    if (userAlreadyEmail) {
+      throw new AppError("This email is already in use");
+    }
+
+    // ----- criando um novo usuário -----
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashPassword,
+      },
     });
+
+    return response.json(user);
   }
 }
