32f97f427dba244e8bae0fd952efa60ea90ddc36
eomgn <matheeusgn21@gmail.com>
2025-08-15 09:01:38 -0300

    :recycle: refactor: removendo o middleware de ensure authenticated do sessions-routes pois estava sendo usdo pra testar

--- src/routes/sessions-routes.ts
+++ src/routes/sessions-routes.ts
@@ -6,8 +6,6 @@ import { SessionsController } from "@/controllers/sessions-controller";
 const sessionsController = new SessionsController();
 
 // middlewares
-import { ensureAuthenticated } from "@/middlewares/ensure-authenticated"; // middle de autenticacao
-sessionsRoutes.use(ensureAuthenticated);
 
 // routes
 sessionsRoutes.post("/", sessionsController.create);
