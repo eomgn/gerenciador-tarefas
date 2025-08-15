3fcd428bbe175fa6c6ea6d89ea5a920847311edf
eomgn <matheeusgn21@gmail.com>
2025-08-15 12:55:41 -0300

    :sparkles: feat: ajustado o método create do teams-controller onde agora é possível cadastrar um novo team passando entao pela validacao com zod, verificacao se ja existe um team cadastrado com o name passado e entao o cadastrado

--- src/controllers/teams-controller.ts
+++ src/controllers/teams-controller.ts
@@ -1,8 +1,32 @@
 import { Request, Response, NextFunction } from "express";
+import zod from "zod";
+import { prisma } from "@/database/prisma";
 import { AppError } from "@/utils/AppError";
 
 export class TeamsController {
-  create(request: Request, response: Response, next: NextFunction) {
-    return response.json({ message: "teams ok!" });
+  async create(request: Request, response: Response, next: NextFunction) {
+    const bodySchema = zod.object({
+      name: zod.string().min(3),
+      description: zod.string(),
+    });
+
+    const { name, description } = bodySchema.parse(request.body);
+
+    // ----- verificando se já existe um time com o mesmo 'name' passado no corpo da requisição -----
+    const teamAlreadyName = await prisma.team.findFirst({ where: { name } });
+
+    if (teamAlreadyName) {
+      throw new AppError("This name is already in use");
+    }
+
+    // ----- cadastrando novo team -----
+    const team = await prisma.team.create({
+      data: {
+        name,
+        description,
+      },
+    });
+
+    return response.json(team);
   }
 }
