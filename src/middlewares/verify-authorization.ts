e81bcd61735e502f87173bd68d8667a688c647d4
eomgn <matheeusgn21@gmail.com>
2025-08-15 10:12:57 -0300

    :sparkles: feat: criando middleware para autorizacao - criado um arquivo verify authorization e criado a funcao para verificar se existe um usuario e esta autenticado em seguida utilizado o middleware na rota de teams

---
+++ src/middlewares/verify-authorization.ts
@@ -0,0 +1,19 @@
+import { Request, Response, NextFunction } from "express";
+import { AppError } from "@/utils/AppError";
+
+export function verifyAuthorization(role: string[]) {
+  return (request: Request, response: Response, next: NextFunction) => {
+    // ----- verificando se o usuário existe, se não tiver usuário no request, significa que ninguém está autenticado -----
+    if (!request.user) {
+      throw new AppError("Unauthorized", 401);
+    }
+
+    // ----- verificando se a role passada na chamada da função é uma role válida de acordo com o enum na criação da table -----
+    if (!role.includes(request.user.role)) {
+      throw new AppError("Unauthorized", 401);
+    }
+
+    //  ----- next para caso passar nas validações a execução segue para o próximo middleware ou controller -----
+    next();
+  };
+}
--- src/routes/teams-routes.ts
+++ src/routes/teams-routes.ts
@@ -6,10 +6,13 @@ import { TeamsController } from "@/controllers/teams-controller";
 const teamsController = new TeamsController();
 
 // middlewares
-import { ensureAuthenticated } from "@/middlewares/ensure-authenticated";
+import { ensureAuthenticated } from "@/middlewares/ensure-authenticated"; // autenticacao
 teamsRoutes.use(ensureAuthenticated);
 
+import { verifyAuthorization } from "@/middlewares/verify-authorization"; // autorização
+
 // routes
-teamsRoutes.post("/", teamsController.create);
+
+teamsRoutes.post("/", verifyAuthorization(["admin"]), teamsController.create);
 
 export { teamsRoutes };
