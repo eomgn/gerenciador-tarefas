854a341844b29213d549af850abec63e668edfa3
eomgn <matheeusgn21@gmail.com>
2025-08-18 11:38:35 -0300

    :sparkles: feat: adicionado em team-member-routes os middlewares de autenticacao e autorizacao bem como tambem definido na rota do tipo post o uso do middleware verifyAuthorization para apenas quem tem a role ADMIN conseguir realizar essa requisicao

--- src/routes/team-member-routes.ts
+++ src/routes/team-member-routes.ts
@@ -1,13 +1,24 @@
 import { Router } from "express";
 const teamMemberRoutes = Router();
 
+// enums
+import { Roles } from "@/enums/roles.enum";
+
 // controller
 import { TeamMembersController } from "@/controllers/team-members-controller";
 const teamMembersController = new TeamMembersController();
 
 // middlewares
+import { ensureAuthenticated } from "@/middlewares/ensure-authenticated";
+teamMemberRoutes.use(ensureAuthenticated);
+
+import { verifyAuthorization } from "@/middlewares/verify-authorization";
 
 // rotas
-teamMemberRoutes.post("/", teamMembersController.create);
+teamMemberRoutes.post(
+  "/",
+  verifyAuthorization([Roles.ADMIN]),
+  teamMembersController.create
+);
 
 export { teamMemberRoutes };
