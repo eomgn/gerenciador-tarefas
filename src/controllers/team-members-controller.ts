223f31046f217a980123e8f86048956eef926103
eomgn <matheeusgn21@gmail.com>
2025-08-18 10:45:44 -0300

    :sparkles: feat: definido o metodo create de team-members-controller onde é feito validacoes alem se realmente existem o user_id e o team_id passados no body da requisicao bem como tambem se já não há um registro de user_id e team_id igualmente na tabela teamMember

--- src/controllers/team-members-controller.ts
+++ src/controllers/team-members-controller.ts
@@ -1,8 +1,55 @@
 import { Request, Response, NextFunction } from "express";
+import { prisma } from "@/database/prisma";
+import zod from "zod";
 import { AppError } from "@/utils/AppError";
 
 export class TeamMembersController {
-  create(request: Request, response: Response, next: NextFunction) {
-    return response.json({ message: "team member ok!!!" });
+  async create(request: Request, response: Response, next: NextFunction) {
+    const bodySchema = zod.object({
+      user_id: zod.uuid(),
+      team_id: zod.uuid(),
+    });
+
+    const { user_id, team_id } = bodySchema.parse(request.body);
+
+    // ----- verificando se o 'user_id' realmente existe no banco de dados, ou seja se o usuario existe -----
+    const userExists = await prisma.user.findUnique({
+      where: { id: user_id },
+    });
+
+    if (!userExists) {
+      throw new AppError("Users not exists", 404);
+    }
+
+    // ----- verificando se o 'team_id' realmente existe no banco de dados, ou seja se o team existe -----
+    const teamExists = await prisma.team.findUnique({
+      where: { id: team_id },
+    });
+
+    if (!teamExists) {
+      throw new AppError("Team not exists", 404);
+    }
+
+    // ----- verificando se o 'user_id' passado no corpo já está no mesmo 'team_id' -----
+    const teamsMembers = await prisma.teamMember.findFirst({
+      where: {
+        userId: user_id,
+        teamId: team_id,
+      },
+    });
+
+    if (teamsMembers) {
+      throw new AppError("User already registered on this team ", 404);
+    }
+
+    // ----- realizando registro de um 'user' especifico a um 'team' especifico ----
+    const teamMemberRegister = await prisma.teamMember.create({
+      data: {
+        userId: user_id,
+        teamId: team_id,
+      },
+    });
+
+    return response.status(201).json(teamMemberRegister);
   }
 }
