fe7d2a8ab6652249ac8c2208ffdfe8c78ee46ac6
eomgn <matheeusgn21@gmail.com>
2025-08-23 14:33:55 -0300

    initial create controller tasks

--- src/controllers/tasks-controller.ts
+++ src/controllers/tasks-controller.ts
@@ -1,8 +1,21 @@
 import { Request, Response, NextFunction } from "express";
+import zod from "zod";
 import { AppError } from "@/utils/AppError";
 
 export class TasksController {
-  create(request: Request, response: Response, next: NextFunction) {
+  async create(request: Request, response: Response, next: NextFunction) {
+    const bodySchema = zod.object({
+      title: zod.string().min(3),
+      description: zod.string().min(3),
+      status: zod.string(),
+      priority: zod.string(),
+      assigned_to: zod.string(),
+      team_id: zod.string(),
+    });
+
+    const { title, description, status, priority, assigned_to, team_id } =
+      bodySchema.parse(request.body);
+
     return response.json({
       message: "create tasks",
     });
