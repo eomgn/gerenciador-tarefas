b43cfb0a3ba1902a76f72e957eb0cc225c0f51db
eomgn <matheeusgn21@gmail.com>
2025-08-24 15:32:10 -0300

    :sparkles: feat: criado o create em tasks-controller com verificacoes

--- src/controllers/tasks-controller.ts
+++ src/controllers/tasks-controller.ts
@@ -1,23 +1,61 @@
 import { Request, Response, NextFunction } from "express";
 import zod from "zod";
+import { prisma } from "@/database/prisma";
 import { AppError } from "@/utils/AppError";
 
 export class TasksController {
   async create(request: Request, response: Response, next: NextFunction) {
     const bodySchema = zod.object({
-      title: zod.string().min(3),
-      description: zod.string().min(3),
-      status: zod.string(),
-      priority: zod.string(),
-      assigned_to: zod.string(),
-      team_id: zod.string(),
+      title: zod.string().trim().min(3),
+      description: zod.string(),
+      assigned_to: zod.uuid().optional(),
+      team_id: zod.uuid().optional(),
     });
 
-    const { title, description, status, priority, assigned_to, team_id } =
-      bodySchema.parse(request.body);
+    const { title, description, assigned_to, team_id } = bodySchema.parse(
+      request.body
+    );
 
-    return response.json({
-      message: "create tasks",
+    // ----- verificando se existe o 'assigned_to' que é o id do usuario e o 'team_id'
+    if (!assigned_to) {
+      throw new AppError("Member not found", 404);
+    }
+
+    if (!team_id) {
+      throw new AppError("Team not found", 404);
+    }
+
+    // ----- verificando se o 'assigned_to' é um membro do 'team_id' informado -----
+    const teamMember = await prisma.teamMember.findFirst({
+      where: {
+        teamId: team_id,
+        userId: assigned_to,
+      },
     });
+
+    if (!teamMember) {
+      throw new AppError("This user does not belong to this team", 404);
+    }
+
+    // ----- verificando se o 'title' já é existente ----
+    const titleExists = await prisma.tasks.findFirst({
+      where: { title },
+    });
+
+    if (titleExists) {
+      throw new AppError("This title is already in use", 400);
+    }
+
+    // ----- cadastrando task -----
+    const task = await prisma.tasks.create({
+      data: {
+        title,
+        description,
+        assigned_to,
+        team_id,
+      },
+    });
+
+    return response.json(task);
   }
 }
--- src/routes/tasks-routes.ts
+++ src/routes/tasks-routes.ts
@@ -1,13 +1,23 @@
 import { Router } from "express";
 const tasksRouter = Router();
 
+// enums
+import { Roles } from "@/enums/roles.enum";
+
 // controllers
 import { TasksController } from "@/controllers/tasks-controller";
 const tasksController = new TasksController();
 
 // middlewares
+import { ensureAuthenticated } from "@/middlewares/ensure-authenticated"; // autenticacao
+import { verifyAuthorization } from "@/middlewares/verify-authorization"; // autorização
 
 // rotas
-tasksRouter.post("/", tasksController.create);
+tasksRouter.post(
+  "/",
+  ensureAuthenticated,
+  verifyAuthorization([Roles.ADMIN]),
+  tasksController.create
+);
 
 export { tasksRouter };
